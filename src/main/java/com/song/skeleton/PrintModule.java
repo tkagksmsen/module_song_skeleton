6e092d937db8b738e06520835740c05fccf28523
xpowerman <xpowerman@tmon.co.kr>
2016-03-13 14:28:53 +0900

    first commit

---
+++ src/main/java/com/song/skeleton/PrintModule.java
@@ -0,0 +1,10 @@
+package com.song.skeleton;
+
+/**
+ * Created by xpowerman on 2016. 3. 13..
+ */
+public class PrintModule {
+    public void print() {
+        System.out.println("skeleton module");
+    }
+}
